a67da41158333fe17361ac30ff39a0ca94e7dbcc
Hogan Haake <hoganhaake@deckis.com>
2013-10-11 14:35:26 -0500

    My commit

--- ClassLibrary2/aaa1.cs
+++ ClassLibrary2/aaa1.cs
@@ -8,5 +8,15 @@ namespace ClassLibrary2
 {
     class aaa1
     {
+        public aaa1()
+        {
+            int x = 0;
+            x++;
+        }
+
+        public int CountUpOne(int number)
+        {
+            return number++;
+        }
     }
 }
